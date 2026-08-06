418816e53fcdf38434c56a36eaaf8be8621b2937
Anna <annposhtak@gmail.com>
2026-08-06 11:38:28 +0300

    feat(frontend): update routing structure and separate layouts with/without header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-import Home from "./pages/Home/Home"
-import FeaturesPage from "./pages/Features/Features"
-import { Header } from "./components/Header"
-import { Footer } from "./components/Footer"
-import HowItWorksPage from "./pages/HowItWorks/HowItWorks"
-import AboutPage from "./pages/AboutPage/AboutPage"
+import Home from "./pages/Home/Home";
+import FeaturesPage from "./pages/Features/Features";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import HowItWorksPage from "./pages/HowItWorks/HowItWorks";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
-function App() {
+import AuthPage from "./pages/Auth/Auth";
+import OnboardingPage from "./pages/Onboarding/Onboarding";
+import { OAuthSuccess } from "./pages/OAuthSuccess/OAuthSuccess";
+
+function AppContent() {
+  const location = useLocation();
+  const hideHeaderFooterRoutes = ["/auth", "/onboarding", "/oauth-success"];
+  const shouldHideHeaderFooter = hideHeaderFooterRoutes.includes(location.pathname);
 
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/features" element={<FeaturesPage />} />
-          <Route path="/how-it-works" element={<HowItWorksPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {!shouldHideHeaderFooter && <Header />}
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/about" element={<AboutPage />} />
+
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/oauth-success" element={<OAuthSuccess />} />
+      </Routes>
+
+      {!shouldHideHeaderFooter && <Footer />}
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+}
+
+export default App;
