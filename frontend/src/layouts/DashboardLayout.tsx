e4dc3b1a0e7747f4888c66fba852506e7b7c5481
Anna <annposhtak@gmail.com>
2026-08-18 20:22:28 +0300

    chore(DashboardLayout): add new styles for pages

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -5,7 +5,7 @@ export default function DashboardLayout() {
   return (
     <div className="flex min-h-screen bg-background">
       <Sidebar />
-      <main className="flex-1 pl-64 p-8 max-w-7xl">
+      <main className="flex-1 min-w-0 pl-64 p-8">
         <Outlet />
       </main>
     </div>
